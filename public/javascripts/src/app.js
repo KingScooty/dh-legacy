ea9f77a9d78b57f247783983b292a54c4f0067cb
Scotty Vernon <scott.vernon@gmail.com>
2015-05-04 20:48:37 +0100

    Grunt browserify, docker ignore js src, initial routes.

--- public/javascripts/src/app.js
+++ public/javascripts/src/app.js
@@ -1,7 +1,24 @@
-var React = require('react'),
-    Stream = require('./jsx/Stream');
+var React = require('react');
+var Stream = require('./jsx/Stream');
+var Router = require('react-router');
 
-React.renderComponent(
-  <Stream />,
-  document.getElementById('app')
+
+var routes = (
+    <Route name="layout" path="/" handler={Layout}>
+      // <Route name="users" path="search/users" handler={Search} />
+      <Route name="year" path="/:year" handler={} />
+      // <Route name="users" path="search/users" handler={Search} />
+      // <Route name="user" path="/users/:username" handler={UserDetail} />
+      // <DefaultRoute handler={Search} />
+      <Redirect from="/" to="2015" />
+    </Route>
 );
+
+// React.renderComponent(
+//   <Stream />,
+//   document.getElementById('app')
+// );
+
+Router.run(routes, function(Handler) {
+  React.render(<Handler/>, document.getElementById('app'));
+});
