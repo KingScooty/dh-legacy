834fda94be92063bc1ce9f264789aeaa94addb55
Scotty Vernon <scott.vernon@gmail.com>
2015-05-08 14:49:57 +0100

    Initial API routes.

---
+++ routes/api.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function callback(req, res) {
+  res.json({ message: 'hooray! welcome to our api!' });
+});
+
+module.exports = router;
