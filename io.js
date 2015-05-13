8b69242f78338974d324d9b8e766cc779002ce2b
Scotty Vernon <scott.vernon@gmail.com>
2015-05-13 14:44:09 +0100

    Don't depend on a connection to trigger twitter stream. Silly.

--- io.js
+++ io.js
@@ -38,28 +38,27 @@ module.exports = function(server){
     throw err;
   });
 
+  var stream = T.stream('statuses/filter', { track: '#digitalheroes2015' });
+
+  stream.on('tweet', function callback(tweet) {
+    console.log('SOMEONE TWEETED!');
+    app.locals.db.dh_2015.save(tweet.id_str, tweet, function callback(err, res) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Tweet ' + tweet.id_str + ' saved to db');
+      }
+    });
+
+    io.sockets.emit('incomingTweet', {'id': tweet.id_str, 'value': tweet});
+  });
+
   // Set Socket.io listeners by creating a socket.io middleware
   // attachEventlisteners would live in `/controllers`
   // io.use(attachEventlisteners);
 
-  io.on('connection', function (socket) {
+  io.sockets.on('connection', function (socket) {
     console.log('SOMEONE IS CONNECTED!');
-
-    var stream = T.stream('statuses/filter', { track: '#digitalheroes2015' });
-
-    stream.on('tweet', function callback(tweet) {
-      console.log('SOMEONE TWEETED!');
-      app.locals.db.dh_2015.save(tweet.id_str, tweet, function callback(err, res) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log('Tweet ' + tweet.id_str + ' saved to db');
-        }
-      });
-
-      io.sockets.emit('incomingTweet', {'id': tweet.id_str, 'value': tweet});
-    });
-
   });
 
   return io; // so it can be used in app.js ( if need be )
