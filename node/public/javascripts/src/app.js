eb85dca1e352bbee6f9f4b8e0244d809007f6c07
Scotty Vernon <scott.vernon@gmail.com>
2015-10-22 20:14:39 +0100

    Block render for fonts. Maintain scroll between years.

--- node/public/javascripts/src/app.js
+++ node/public/javascripts/src/app.js
@@ -42,6 +42,60 @@ var eventInfoRoutes = (
 //   document.getElementById('app')
 // );
 
+var target = document.querySelector('.event');
+var current_event_height = 0;
+var pageInitialised = false;
+
+var getInitialEventHeight = function getInitialEventHeight() {
+  return window.dh_el_event_current_height;
+}
+
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+
+    if (!pageInitialised) {
+      current_event_height = getInitialEventHeight();
+      console.log('FIRST HEIGHT: ', current_event_height);
+      pageInitialised = true;
+    }
+
+    // console.log(mutation.type);
+    // console.log(target.offsetHeight);
+    // current_event_height = window.dh_el_event_current_height;
+    var new_event_height = target.clientHeight;
+    var diff_event_height = new_event_height - current_event_height;
+
+    console.log('Current height: ',current_event_height);
+    console.log('New height: ', new_event_height);
+    console.log('Diff height: ', diff_event_height);
+
+    current_event_height += diff_event_height;
+
+    console.log('Current scroll: ', document.body.scrollTop);
+    console.log('Scroll modifier: ', diff_event_height);
+    document.body.scrollTop += diff_event_height;
+    console.log('New scroll: ', document.body.scrollTop);
+  });
+});
+
+// configuration of the observer:
+var config = {
+  // attributes: true,
+  // childList: true,
+  // characterData: true,
+  // type: true
+  // attributes: true,
+  // childList: true,
+  characterData: true,
+  subtree:true
+};
+
+// pass in the target node, as well as the observer options
+observer.observe(target, config);
+
+// // later, you can stop observing
+// observer.disconnect();
+
 Router.run(routes, Router.HistoryLocation, function(Handler) {
   React.render(<Handler/>, document.getElementById('app'));
 });
