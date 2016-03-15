207b9d7bdee8fa5f4653b8f97ba79715076d02d5
Scotty Vernon <scott.vernon@gmail.com>
2016-03-15 23:35:42 +0000

    Add template for core_spec.

---
+++ api/test/core_spec.js
@@ -0,0 +1,24 @@
+/*eslint-disable */
+var supertest = require('supertest');
+var chai = require('chai');
+
+var expect = chai.expect;
+chai.should();
+
+const request = supertest.agent(app.listen());
+const context = {};
+
+describe('API', () => {
+  // before((done) => {
+  //   done();
+  // });
+
+  describe('', () => {
+    it('', (done) => {});
+  });
+
+  describe('', () => {
+    it('', (done) => {});
+  });
+
+});
