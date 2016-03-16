7fa170b084de6e2377d2ae632dcbf38c9ab36b0e
Scotty Vernon <scott.vernon@gmail.com>
2016-03-16 23:21:27 +0000

    More comments.

--- api/db.js
+++ api/db.js
@@ -10,6 +10,20 @@ var c = new (cradle.Connection)(host, 5984, {
   auth: auth
 });
 
+/*
+  http://couchdb-3c369e79-1.kingscooty.cont.tutum.io:5984/_all_dbs
+
+  ["_replicator","_users","digitalheroes-2012","digitalheroes-2013","digitalheroes-2014","digitalheroes-2015","digitalheroes-halloween-2015"]
+
+  May be worth querying what databases exist, and then mounting them instead of storing the object below??
+
+  This way would allow for creation of new databases programatically when there's a new event. Would the worker create the new entry?
+
+  The API should allow for the creation of databases from an end point, perhaps?
+
+  Would have to be a secure endpoint.
+*/
+
 var db = {
   dh_2012: c.database('digitalheroes-2012'),
   dh_2013: c.database('digitalheroes-2013'),
