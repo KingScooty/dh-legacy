54d00e76fc1423c95ecfc133e7cd3742d476826e
Scotty Vernon <scott.vernon@gmail.com>
2016-03-16 17:30:22 +0000

    Flesh out error handling middleware

--- api/middleware/errors.js
+++ api/middleware/errors.js
@@ -1,34 +1,35 @@
 const Promise = require('bluebird');
 const co = Promise.coroutine;
-
-// app.use(function *(next) {
-//   try {
-//     yield next;
-//   } catch (err) {
-//     this.status = err.status || 500;
-//     this.body = err.message;
-//     this.app.emit('error', err, this);
-//   }
-// });
-
+const http = require('http');
 
 module.exports = function errorMiddleware() {
-  // app.use( (ctx, next) => {
   return co(function *(ctx, next) {
+
+    // env
+    const env = process.env.NODE_ENV || 'development';
+
     try {
       yield next();
+      if (ctx.response.status && !ctx.response.body === 404) ctx.throw(404);
     } catch (err) {
+
+      // some errors will have .status
+      // however this is not a guarantee
+      ctx.status = err.status || 500;
+
       if (err === null) {
         err = new Error('Null or undefined error');
       }
-      // some errors will have .status
-      // however this is not a guarantee
-      this.status = err.status || 500;
-      this.type = 'application/json';
-      this.body = JSON.stringify({
-        success: false,
-        message: err.stack,
-      });
+
+      ctx.type = 'application/json';
+      if (env === 'development') ctx.body = { error: err.message }
+      else if (err.expose) this.body = { error: err.message }
+      else this.body = { error: http.STATUS_CODES[ctx.status]}
+
+      // ctx.body = JSON.stringify({
+      //   success: false,
+      //   message: err.stack,
+      // });
 
       // since we handled this manually we'll
       // want to delegate to the regular app
@@ -36,6 +37,5 @@ module.exports = function errorMiddleware() {
       // centralized still functions correctly.
       ctx.app.emit('error', err, ctx);
     }
-  }); //();
-  // });
+  });
 };
