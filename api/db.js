ac76d7c4d1613c0be1fcfc8d50495eca1bec3fcc
Scotty Vernon <scott.vernon@gmail.com>
2016-03-19 10:58:32 +0000

    Scrap off centralised db funcs and move to model.

--- api/db.js
+++ api/db.js
@@ -24,7 +24,7 @@ var c = new (cradle.Connection)(host, 5984, {
   Would have to be a secure endpoint.
 */
 
-var db = {
+var databaseList = {
   dh_2012: c.database('digitalheroes-2012'),
   dh_2013: c.database('digitalheroes-2013'),
   dh_2014: c.database('digitalheroes-2014'),
@@ -32,103 +32,5 @@ var db = {
   dh_halloween15: c.database('digitalheroes-halloween-2015')
 };
 
-/**
- * Creates a new database
- *
- * @param {Object} database
- * @returns a promise??????
- */
-
-
-// exports.createDB = function(database) {
-//   // should really return a promise.
-//   database.create(function(err, res) {
-//     if (err) return err;
-//     return res;
-//   });
-// }
-
-exports.saveDoc = function() {}
-exports.allDocs = function() {}
-
-/**
- * Creates a view in the database
- *
- * @param {Object} database
- * @param {Function} callback
- * @returns {Function} callback
- */
-
-exports.createDefaultView = function (database, callback) {
-
-  console.log('Creating database view');
-
-  db.save('_design/tweets', {
-    all: {
-      map: function (doc) {
-        if (doc._id) emit(doc._id, doc);
-      }
-    },
-    all_tweets: {
-      map: function (doc) {
-        if (doc.type === 'tweet') emit(doc._id, doc);
-      }
-    },
-    event_info: {
-      map: function (doc) {
-        if (doc.type === 'info') emit(doc._id, doc);
-      }
-    }
-  }, function(err, res) {
-    if (err) {
-      console.log('error', err);
-    } else {
-      callback();
-      //TODO: Swap this for a promise;
-    }
-  });
-}
-
-/**
- * Queries database via a view, and returns response.
- *
- * @param {String} view
- * @param {Array} options
- * @returns
- */
-
-exports.getView = function(view, options) {
-  db.view(view, options, function callback(err, response) {});
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var url = `${username}:${password}@${host}:5984/`
-// var db = 'digitalheroes-2012';
-//
-// // save a document
-// exports.save = function(db, doc, done) {
-//   request.put({
-//     url: `${url}/${db}/${doc._id}`,
-//     body: doc,
-//     json: true,
-//   }, function(err, resp, body) {
-//     if (err) return done('Unable to connect to CouchDB');
-//     if (body.ok) {
-//       doc._rev = body.rev;
-//       return done(null doc);
-//     }
-//
-//     done('Unable to save the document');
-//   });
-// }
+module.exports.connection = c;
+module.exports.databaseList = databaseList;
--- api/models/events/index.js
+++ api/models/events/index.js
@@ -1,8 +1,180 @@
+var cradle = require('cradle');
+var Promise = require('bluebird');
 var db = require('../../db');
 
-views = {
-  all: 'tweets/all_tweets'
+var lastInObject = require('../../helpers/last_in_object');
+
+var EventModel = function EventModel() {
+  this.connection = db.connection;
+  this.databaseList = db.databaseList;
+  this.defaultDatabase = lastInObject(db.databaseList);
+};
+
+/**
+ * Saves the latest design document to database
+ *
+ * @param {String} db - 'dh2015' / 'dh_halloween15'
+ * @param {Function} callback
+ */
+
+
+EventModel.prototype.syncDesignDoc =
+function syncDesignDoc(db, callback) {
+
+  var database;
+
+  if (typeof db === "string" || db instanceof String) {
+    database = this.databaseList[database]
+  } else {
+    database = this.defaultDatabase;
+  }
+
+  database.save('_design/tweets', {
+    all: {
+      map: function (doc) {
+        if (doc._id) emit(doc._id, doc);
+      }
+    },
+    all_tweets: {
+      map: function (doc) {
+        if (doc.type === 'tweet') emit(doc._id, doc);
+      }
+    },
+    event_info: {
+      map: function (doc) {
+        if (doc.type === 'info') emit(doc._id, doc);
+      }
+    }
+  }, function(err, res) {
+    if (err) {
+      console.log('error', err);
+    } else {
+      callback();
+      //TODO: Swap this for a promise;
+    }
+  });
+};
+
+/**
+ * Returns all the documents from a database
+ *
+ * @param {String} db - 'dh2015' / 'dh_halloween15'
+ * @param {Function} callback
+ */
+
+EventModel.prototype.findAll =
+function findAll(db, callback) {
+
+  if (typeof db === "string" || db instanceof String) {
+    database = this.databaseList[database]
+  } else {
+    database = this.defaultDatabase;
+  }
+
+  // return new Promise(function(resolve) {
+    database.view('tweets/all', function(err, result) {
+      if (err) {
+        callback(error)
+      } else {
+        var docs = [];
+        result.forEach(function (row) {
+          docs.push(row);
+        });
+        // resolve(null, docs);
+        callback(null, docs);
+      }
+    });
+  // });
+
+};
+
+/**
+ * Returns all the documents by type from a database
+ *
+ * @param {String} docType - 'event_info' / 'all_tweets'
+ * @param {Function} callback
+ */
+
+EventModel.prototype.findByType =
+function findByType(db, docType, callback) {
+
+  if (typeof db === "string" || db instanceof String) {
+    database = this.databaseList[database]
+  } else {
+    database = this.defaultDatabase;
+  }
+
+  database.view(`tweets/${docType}`, function(err, result) {
+    if (err) {
+      callback(error)
+    } else {
+      var docs = [];
+      result.forEach(function (row) {
+        docs.push(row);
+      });
+      callback(null, docs);
+    }
+  });
+};
+
+/**
+ * Saves a document (tweet) to the database
+ *
+ * @param {String} docType - 'event_info' / 'all_tweets'
+ * @param {Function} callback
+ */
+
+EventModel.prototype.save =
+function save(db, tweet) {
+
+  if (typeof db === "string" || db instanceof String) {
+    database = this.databaseList[database]
+  } else {
+    database = this.defaultDatabase;
+  }
+
+  database.save(tweet.id_str, tweet, function callback(err, res) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Tweet ${tweet.id_str} saved to db`);
+    }
+  });
 }
+
+/*
+EventModel.prototype.findTweets = function(callback) {
+  this.defaultDB.view('tweets/all_tweets', function(err, result) {
+    if (err) {
+      callback(error)
+    } else {
+      var docs = [];
+      result.forEach(function (row) {
+        docs.push(row);
+      });
+      callback(null, docs);
+    }
+  });
+};
+
+EventModel.prototype.findTweets = function(callback) {
+  this.defaultDB.view('tweets/event_info', function(err, result) {
+    if (err) {
+      callback(error)
+    } else {
+      var docs = [];
+      result.forEach(function (row) {
+        docs.push(row);
+      });
+      callback(null, docs);
+    }
+  });
+};
+*/
+
+
+// exports.EventModel = EventModel;
+module.exports = EventModel;
 
 //
 //
