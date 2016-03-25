6f424b595b925c9ca2d6efafd933c4e9299c7545
Scotty Vernon <scott.vernon@gmail.com>
2016-03-25 15:32:39 +0000

    Cleanup config.

--- api/config/env/development.js
+++ api/config/env/development.js
@@ -1,11 +1,5 @@
 module.exports = {
   database: {
     host: '127.0.0.1:5984'
-  },
-  twitter: {
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
   }
 }
--- api/config/env/test.js
+++ api/config/env/test.js
@@ -1,11 +1,5 @@
 module.exports = {
   database: {
-    host: 'http://admin:BCRmhDz4hT7g@'
-  },
-  twitter: {
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+    host: '127.0.0.1:5984'
   }
 }
