0efaf1587e6a725a264621788e00cab10a04a652
Scotty Vernon <scott.vernon@gmail.com>
2016-03-25 20:59:29 +0000

    Comments on couch updates over socket.

--- api/server.js
+++ api/server.js
@@ -22,6 +22,15 @@ const getServer = () => {
 
   io.on('connection', () => {
     // do stuff with socket
+    /*
+     * Handle couchdb changes and report update to user.
+     * Use the nano updates method.
+     * https://github.com/dscape/nano#nanoupdatesparams-callback
+     *
+     * Change params.feed to continuous or eventsource.
+     * Idea is that couch will report when a new tweet is saved, and push it to
+     * to the end user over this socked.
+     */
   });
 
   return server;
