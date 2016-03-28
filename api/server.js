efe1a62983384d8d30d44936aa898d5eef15d88d
Scotty Vernon <scott.vernon@gmail.com>
2016-03-28 22:13:40 +0100

    Toying with change api.

--- api/server.js
+++ api/server.js
@@ -38,4 +38,12 @@ const getServer = () => {
 
 getServer().listen(1337, () => {
   console.log('HTTP server listening on port 1337');
+  const nano = require('nano')('http://127.0.0.1:5984');
+  var db = nano.use('digitalheroes-halloween-2015');
+  nano.updates({
+    feed: "continuous"
+  }, function(response) {
+    console.log(response);
+    console.log('Wha?');
+  });
 });
