24157023354a5ea05d08e5a51f1603a162c775b9
Scotty Vernon <scott.vernon@gmail.com>
2016-03-29 19:22:53 +0100

    Abstract latest test.

--- api/test/db_spec.js
+++ api/test/db_spec.js
@@ -4,12 +4,15 @@ var expect = chai.expect;
 chai.should();
 
 var db = require('../db').database;
+var lastInObject = require('../helpers/last_in_object');
 
 describe('Database', () => {
   describe('latest()', () => {
     it('should return the latest database', () => {
+      var dbList = require('../db').databaseList;
       var latest = db.latest();
-      expect(latest.config.db).to.equal('digitalheroes-halloween-2015');
+      var expected = lastInObject(dbList);
+      expect(latest.config.db).to.equal(expected.config.db);
     });
   });
   describe('year()', () => {
